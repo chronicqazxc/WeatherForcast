8f88b195c59e7d5748e04e21a36c81a12efe9a1d
Wayne Hsiao <chronicqazxc@gmail.com>
2026-03-08 13:05:11 +0900

    Initial Commit

---
+++ WeatherForcast/WeatherForcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForcastApp.swift
+//  WeatherForcast
+//
+//  Created by WayneHsiao on 2026/03/08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
